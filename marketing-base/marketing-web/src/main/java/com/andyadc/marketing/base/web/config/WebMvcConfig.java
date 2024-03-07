a9e7292bab7b66824cc526c72121945c285f4514
andyadc <jjwadc@gmail.com>
2024-03-07 10:17:21 +0800

    web config

---
+++ marketing-base/marketing-web/src/main/java/com/andyadc/marketing/base/web/config/WebMvcConfig.java
@@ -0,0 +1,6 @@
+package com.andyadc.marketing.base.web.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+}
