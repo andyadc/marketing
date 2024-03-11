0950068cde9b67e41d8c5efa6e4ddaea093c82fc
andyadc <jjwadc@gmail.com>
2024-03-11 09:50:25 +0800

    marketing

---
+++ tiny-marketing/src/test/java/com/andyadc/marketing/test/MarketingTests.java
@@ -0,0 +1,12 @@
+package com.andyadc.marketing.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MarketingTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
