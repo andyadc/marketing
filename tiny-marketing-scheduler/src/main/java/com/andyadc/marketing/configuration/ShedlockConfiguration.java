c30811d8f3b56ffcc047816159f4a357fabd9799
andyadc <jjwadc@gmail.com>
2024-05-16 14:22:49 +0800

    marketing scheduler

--- tiny-marketing-scheduler/src/main/java/com/andyadc/marketing/configuration/ShedlockConfiguration.java
+++ tiny-marketing-scheduler/src/main/java/com/andyadc/marketing/configuration/ShedlockConfiguration.java
@@ -2,11 +2,13 @@ package com.andyadc.marketing.configuration;
 
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
+@EnableSchedulerLock(defaultLockAtMostFor = "PT60S")
 @Configuration
 public class ShedlockConfiguration {
 
