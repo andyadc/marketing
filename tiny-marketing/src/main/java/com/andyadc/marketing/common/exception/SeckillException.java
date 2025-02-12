b52efc0be36418ceb2bef10b17d6eb616fb038b3
andyadc <jjwadc@gmail.com>
2025-02-12 09:11:18 +0800

    seckill

---
+++ tiny-marketing/src/main/java/com/andyadc/marketing/common/exception/SeckillException.java
@@ -0,0 +1,36 @@
+package com.andyadc.marketing.common.exception;
+
+import com.andyadc.marketing.common.ErrorCode;
+
+public class SeckillException extends RuntimeException {
+
+	private static final long serialVersionUID = 6072087408074461087L;
+
+	private String code;
+
+	public SeckillException(ErrorCode errorCode) {
+		this(errorCode.code(), errorCode.message());
+	}
+
+	public SeckillException(ErrorCode errorCode, Throwable cause) {
+		this(errorCode.code(), errorCode.message(), cause);
+	}
+
+	public SeckillException(String code, String messgae) {
+		super(messgae);
+		this.code = code;
+	}
+
+	public SeckillException(String code, String messgae, Throwable cause) {
+		super(messgae, cause);
+		this.code = code;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+}
---
+++ tiny-marketing/src/main/java/com/andyadc/marketing/common/response/ResponseMessageBuilder.java
@@ -0,0 +1,22 @@
+package com.andyadc.marketing.common.response;
+
+import com.andyadc.marketing.common.ErrorCode;
+
+public final class ResponseMessageBuilder {
+
+	public static <T> ResponseMessage<T> success(T body) {
+		return build(ErrorCode.SUCCESS.code(), ErrorCode.SUCCESS.message(), body);
+	}
+
+	public static <T> ResponseMessage<T> build(String code, String message) {
+		return build(code, message, null);
+	}
+
+	public static <T> ResponseMessage<T> build(String code, String message, T body) {
+		return new ResponseMessage<T>(code, message, body);
+	}
+
+	public static <T> ResponseMessage<T> build(String code) {
+		return new ResponseMessage<T>(code);
+	}
+}
