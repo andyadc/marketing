f669dab85b09bc5ff78d179a2f11f0ad3bd0debc
andyadc <jjwadc@gmail.com>
2025-03-11 09:04:52 +0800

    group-buying model

---
+++ tiny-marketing/src/main/java/com/andyadc/marketing/domain/group/activity/model/entity/TrialBalanceEntity.java
@@ -0,0 +1,4 @@
+package com.andyadc.marketing.domain.group.activity.model.entity;
+
+public class TrialBalanceEntity {
+}
---
+++ tiny-marketing/src/main/java/com/andyadc/marketing/domain/group/activity/model/valobj/GroupBuyingActivityDiscountVO.java
@@ -0,0 +1,4 @@
+package com.andyadc.marketing.domain.group.activity.model.valobj;
+
+public class GroupBuyingActivityDiscountVO {
+}
