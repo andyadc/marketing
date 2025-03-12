e2b53b7b7677974df9bc0b6b920d16b5df567562
andyadc <jjwadc@gmail.com>
2025-03-12 09:02:17 +0800

    group-buying model

--- tiny-marketing/src/main/java/com/andyadc/marketing/domain/group/activity/model/entity/MarketProductEntity.java
+++ tiny-marketing/src/main/java/com/andyadc/marketing/domain/group/activity/model/entity/MarketProductEntity.java
@@ -1,4 +1,66 @@
 package com.andyadc.marketing.domain.group.activity.model.entity;
 
 public class MarketProductEntity {
+
+	/**
+	 * 活动ID
+	 */
+	private Integer activityId;
+	/**
+	 * 用户ID
+	 */
+	private String userId;
+	/**
+	 * 商品ID
+	 */
+	private String goodsId;
+	/**
+	 * 渠道
+	 */
+	private String source;
+	/**
+	 * 来源
+	 */
+	private String channel;
+
+	public Integer getActivityId() {
+		return activityId;
+	}
+
+	public void setActivityId(Integer activityId) {
+		this.activityId = activityId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getGoodsId() {
+		return goodsId;
+	}
+
+	public void setGoodsId(String goodsId) {
+		this.goodsId = goodsId;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getChannel() {
+		return channel;
+	}
+
+	public void setChannel(String channel) {
+		this.channel = channel;
+	}
+
 }
