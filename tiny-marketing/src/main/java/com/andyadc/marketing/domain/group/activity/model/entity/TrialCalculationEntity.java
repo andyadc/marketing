6fb1eded23d5249025c5bee950e2aa031ad05b8b
andaicheng <andaicheng@gmail.com>
2025-07-26 17:51:36 +0800

    optimization

--- tiny-marketing/src/main/java/com/andyadc/marketing/domain/group/activity/model/entity/TrialCalculationEntity.java
+++ tiny-marketing/src/main/java/com/andyadc/marketing/domain/group/activity/model/entity/TrialCalculationEntity.java
@@ -1,4 +1,16 @@
 package com.andyadc.marketing.domain.group.activity.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 试算结果实体对象
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TrialCalculationEntity {
 }
