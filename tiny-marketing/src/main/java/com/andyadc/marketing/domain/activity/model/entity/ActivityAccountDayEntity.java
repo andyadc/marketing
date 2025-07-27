ab0164f3f98da450eb4ced3f16daf4ee9eb1b7c7
andaicheng <andaicheng@gmail.com>
2025-07-27 12:45:45 +0800

    optimization

--- tiny-marketing/src/main/java/com/andyadc/marketing/domain/activity/model/entity/ActivityAccountDayEntity.java
+++ tiny-marketing/src/main/java/com/andyadc/marketing/domain/activity/model/entity/ActivityAccountDayEntity.java
@@ -1,7 +1,16 @@
 package com.andyadc.marketing.domain.activity.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * 活动账户（日）实体对象
  */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ActivityAccountDayEntity {
 }
--- tiny-marketing/src/main/java/com/andyadc/marketing/domain/activity/model/valobj/ActivitySkuStockKeyVO.java
+++ tiny-marketing/src/main/java/com/andyadc/marketing/domain/activity/model/valobj/ActivitySkuStockKeyVO.java
@@ -1,7 +1,16 @@
 package com.andyadc.marketing.domain.activity.model.valobj;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * 活动sku库存 key 值对象
  */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ActivitySkuStockKeyVO {
 }
