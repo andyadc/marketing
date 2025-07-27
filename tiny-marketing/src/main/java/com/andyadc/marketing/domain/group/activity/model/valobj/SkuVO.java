dc0df4f71ca9f024395b301f94ea91c78f5178da
andaicheng <andaicheng@gmail.com>
2025-07-27 11:59:00 +0800

    optimization

--- tiny-marketing/src/main/java/com/andyadc/marketing/domain/group/activity/model/valobj/SkuVO.java
+++ tiny-marketing/src/main/java/com/andyadc/marketing/domain/group/activity/model/valobj/SkuVO.java
@@ -1,4 +1,13 @@
 package com.andyadc.marketing.domain.group.activity.model.valobj;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SkuVO {
 }
