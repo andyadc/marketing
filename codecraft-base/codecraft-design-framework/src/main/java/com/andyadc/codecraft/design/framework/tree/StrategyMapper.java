09b5130cfd833e14f6a012a23a5e4834b710c772
andyadc <jjwadc@gmail.com>
2025-08-22 09:21:21 +0800

    design.framework.tree

---
+++ codecraft-base/codecraft-design-framework/src/main/java/com/andyadc/codecraft/design/framework/tree/StrategyMapper.java
@@ -0,0 +1,21 @@
+package com.andyadc.codecraft.design.framework.tree;
+
+/**
+ * 策略映射器
+ *
+ * @param <T> 入参类型
+ * @param <D> 上下文参数
+ * @param <R> 返参类型
+ */
+public interface StrategyMapper<T, D, R> {
+
+	/**
+	 * 获取待执行策略
+	 *
+	 * @param requestParameter 入参
+	 * @param dynamicContext   上下文
+	 * @return 返参
+	 * @throws Exception 异常
+	 */
+	StrategyHandler<T, D, R> get(T requestParameter, D dynamicContext) throws Exception;
+}
