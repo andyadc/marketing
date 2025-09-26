5a9046590d9798fb023ca3bc9091806e663b1fe8
andaicheng <andaicheng@gmail.com>
2025-09-26 20:52:46 +0800

    optimization

--- tiny-marketing/src/main/java/com/andyadc/marketing/api/dto/ActivityDrawRequestDTO.java
+++ tiny-marketing/src/main/java/com/andyadc/marketing/api/dto/ActivityDrawRequestDTO.java
@@ -1,12 +1,19 @@
 package com.andyadc.marketing.api.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
 import java.io.Serializable;
 
 /**
  * 活动抽奖请求对象
  */
+@Getter
+@Setter
 public class ActivityDrawRequestDTO implements Serializable {
 
+	@Serial
 	private static final long serialVersionUID = -8889741320371882936L;
 
 	/**
@@ -18,22 +25,6 @@ public class ActivityDrawRequestDTO implements Serializable {
 	 * 活动ID
 	 */
 	private Long activityId;
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public Long getActivityId() {
-		return activityId;
-	}
-
-	public void setActivityId(Long activityId) {
-		this.activityId = activityId;
-	}
 
 	@Override
 	public String toString() {
--- tiny-marketing/src/main/java/com/andyadc/marketing/api/dto/ActivityDrawResponseDTO.java
+++ tiny-marketing/src/main/java/com/andyadc/marketing/api/dto/ActivityDrawResponseDTO.java
@@ -1,12 +1,19 @@
 package com.andyadc.marketing.api.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
 import java.io.Serializable;
 
 /**
  * 活动抽奖返回对象
  */
+@Getter
+@Setter
 public class ActivityDrawResponseDTO implements Serializable {
 
+	@Serial
 	private static final long serialVersionUID = -7424960370842484038L;
 
 	/**
@@ -23,30 +30,6 @@ public class ActivityDrawResponseDTO implements Serializable {
 	 * 排序编号【策略奖品配置的奖品顺序编号】
 	 */
 	private Integer awardIndex;
-
-	public Integer getAwardId() {
-		return awardId;
-	}
-
-	public void setAwardId(Integer awardId) {
-		this.awardId = awardId;
-	}
-
-	public String getAwardTitle() {
-		return awardTitle;
-	}
-
-	public void setAwardTitle(String awardTitle) {
-		this.awardTitle = awardTitle;
-	}
-
-	public Integer getAwardIndex() {
-		return awardIndex;
-	}
-
-	public void setAwardIndex(Integer awardIndex) {
-		this.awardIndex = awardIndex;
-	}
 
 	@Override
 	public String toString() {
