3371b2f392a05b922f4d89b03f0444131935291d
andaicheng <andaicheng@gmail.com>
2025-09-29 17:07:57 +0800

    group

---
+++ tiny-marketing-api/src/main/java/com/andyadc/marketing/api/GroupTradeService.java
@@ -0,0 +1,4 @@
+package com.andyadc.marketing.api;
+
+public interface GroupTradeService {
+}
---
+++ tiny-marketing/src/main/java/com/andyadc/marketing/trigger/http/GroupTradeController.java
@@ -0,0 +1,9 @@
+package com.andyadc.marketing.trigger.http;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/group/v1/trade/")
+@RestController
+public class GroupTradeController {
+}
